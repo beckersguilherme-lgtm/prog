d4fcdfa6c1b2430bee425d357c9fe7075cc94299
beckersguilherme-lgtm <beckers.guilherme@escola.pr.gov.br>
2026-05-29 10:56:01 +0000

    pronto

---
+++ listas/lista 1/exercicico 01.c
@@ -0,0 +1,23 @@
+#include
+#include <string.h>
+
+typedef struct{
+
+    char nome [50];
+    float altura;
+    int idade;
+
+pessoa;
+}
+
+int main(){
+    
+    pessoa p;
+
+    scanf(" %[^\n]", p.nome);
+    printf("nome:  ")
+    scanf("%f", &p.altura);
+    printf("altura:  ")
+    scanf("%d", &p.idade);
+    printf("idade:  ")
+}
